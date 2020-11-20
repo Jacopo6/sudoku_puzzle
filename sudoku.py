36f67a7e64b8f8b66d2cae17ebe5b9b753d9c45e
Jacopo6 <63340231+Jacopo6@users.noreply.github.com>
2020-11-20 11:11:44 +0100

    Create sudoku.py

---
+++ sudoku.py
@@ -0,0 +1,16 @@
+"""
+File: sudoku.py
+---------------
+Insert comment...
+Jacopo Mascitelli
+"""
+
+
+def main():
+    # Use a breakpoint in the code line below to debug your script.
+    print("hello world!")
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
